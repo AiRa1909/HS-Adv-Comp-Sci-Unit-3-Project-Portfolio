002cc6bad20f61adb7c89732be2b2f4c43124a98
AiRa1909 <safa.rasyidin@thekaustschool.org>
2025-12-18 17:45:57 +0300

    Dec 18

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, redirect, url_for
+from flask import Blueprint, redirect, url_for, flash
 from .models import Sesh
 from . import db
 from datetime import datetime
@@ -17,6 +17,9 @@ def stop():
     active = Sesh.query.filter_by(duration=None).first()
     if active:
         diff = datetime.now() - active.start_time
+        mins = round(diff.total_seconds() / 60, 2)
         active.duration = diff.total_seconds() / 60
         db.session.commit()
+
+        flash(f"Congrats, your session ended! You studied for {mins} minutes just now.")
     return redirect(url_for('views.home'))
